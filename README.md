29ceb4f4a93c84abd1feac994745245c26352d73
Lolorisotto <lorisvullion@gmail.com>
2026-04-08 13:41:39 -0400

    Cancelled the writing of system() in stdout - App looks like it's working fine

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Windows desktop app for convert youtube URL to mp3 file. This is an interface of yt-dlp, an actual app not coded by me, to make it more friendly.
 
 # Release - Downloading the app
-You will find the setup file in a .rar file in the release, you will have to unzip it and launch the setup.exe file. Keep in mind that this app have been made for windows only.
+You will find the setup file in a .rar file in the release, you will have to unzip it and launch the setup.exe file. Keep in mind that this app have been made for Windows only. Always choose the latest release.
 
 # Chrome extension
 A chrome extension is also available, it shows a button next to the title of every video on youtube, once clicked the app will launch with the input already filled with the proper url.
@@ -18,7 +18,7 @@ A chrome extension is also available, it shows a button next to the title of eve
 As github don't like big files to make this code work you will need to add a file named src/bin/ with the .exe of ffmpeg and yt-dlp (named ffmpeg.exe and yt-dlp.exe). No need for that if you downloading the app from the release.
 
 It's using basic electron to display web page as desktop app.
-On the first launch the app will automatically register itself in the register so that the extension can communicate with it.
-To do the communication between the extension and app a .exe written in c++ has been made. This app read the stdin, retrieve the url, put it on a .txt file and launch the app if it's not already launch.
+On the first launch the app will automatically register itself so that the extension can communicate with it.
+To do the communication between the extension and the app a .exe written in c++ has been made. This app read the stdin, retrieve the url, put it on a .txt file and launch the app if it's not already launch. You will have to re compile the cpp after any changes before compiling the full app.
 
 The app have a watcher on the .txt file so that if it's changed it retrieve the data and put it in the input.
--- src/host/hostk7.cpp
+++ src/host/hostk7.cpp
@@ -7,8 +7,8 @@
 #include <cstdio>
 #include <Windows.h>
 
-
 int main() {
+    // Set binary mode to avoid corrupt message
     _setmode( _fileno( stdin ), _O_BINARY );
     _setmode(_fileno(stdout), _O_BINARY);
     setvbuf(stdin, NULL, _IONBF, 0);
@@ -16,16 +16,14 @@ int main() {
     // Opening a file for debugging
     std::ofstream ofs("log.txt");
 
-    // Set date in debug
+    // Set date in debug file
     std::time_t now = std::time(nullptr); 
-
     char* dt = std::ctime(&now);
-
     ofs << "hostk7.exe executed - " << dt << std::endl;
 
+    // Initialize variable to get the size of the json (first 4 bytes of the message send)
     int messageSize = 0;
     std::size_t nCount;
-    char * jsonMsg = NULL;
 
     try {
         // Reading message size
@@ -35,9 +33,9 @@ int main() {
         ofs << "Error while reading message size sdtin" << std::endl;
     }
 
-    // Initialize the variable to get the message
+    // Initialize the variable to get the json
     std::vector<char> message(messageSize);
-    std::string contentMessage = "{\"url\" : \"Error if displayed\"}";
+    std::string contentMessage = "{\"url\" : \"Error if displayed\"}"; // Value by default that need to be overwritten by actual json
 
     try {
         // Reading message
@@ -53,13 +51,13 @@ int main() {
         for (int i = 0; i < message.size(); i++) {
             contentMessage += message.at(i);
         }
-       ofs << "Message : "+contentMessage << std::endl;
+        ofs << "Message : "+contentMessage << std::endl;
 
     } catch (...) {
         ofs << "Error while reading message" << std::endl;
     }
 
-    // Creating new file
+    // Creating/openning new file to put the json in
     std::ofstream outfile ("url.txt");
 
     // Adding the url to the file to trigger the watcher on the app
@@ -68,30 +66,31 @@ int main() {
     // Closing file
     outfile.close();
 
-    // Searching of instance of the application
-    //std::string findProcessCmd = "tasklist | findstr \"K7-YTB-MP3\"";
-    //int result = system(findProcessCmd.c_str());
+    // Searching for instance of the application (with stdout output cancelled)
+    std::string findProcessCmd = "tasklist | findstr \"K7-YTB-MP3\" >nul 2>nul";
+    int result = system(findProcessCmd.c_str());
 
-    // If result == 1 no process found
-    //if (result == 1) {
+    // If result == 1 no process found so we start the app
+    if (result == 1) {
 
         STARTUPINFOA si = { sizeof(si) };
         PROCESS_INFORMATION pi;
         
-        // Adding the url as arg
+        // Adding an arg telling the app to check the file where the json msg is
         std::string command ="C:\\Users\\loris\\AppData\\Local\\K7_YTB_MP3\\K7-YTB-MP3.exe --data";
 
+        // Launching the app as an detached process
         CreateProcessA(
             NULL,
             &command[0],
             NULL, NULL, FALSE,
-            DETACHED_PROCESS, // Launched with detached process
+            DETACHED_PROCESS,
             NULL, NULL,
             &si, &pi
         );
         CloseHandle(pi.hProcess);
         CloseHandle(pi.hThread);
-    //}
+    }
 
     // Sending end message to chrome
     std::string endMessageJSON = "{\"status\":\"ok\"}";
@@ -99,11 +98,12 @@ int main() {
     unsigned int len = static_cast<unsigned int>(endMessageJSON.length());
     // Write the length to stdout
     std::cout.write(reinterpret_cast<char*>(&len), 4);
-    // Write the message body
+    // Write the message to stdout
     std::cout.write(endMessageJSON.c_str(), len);
 
     std::cout.flush();
 
+    // Mention end of program in log
     ofs << "End of hostk7.exe" << std::endl;
     ofs.close();
     return 0;
